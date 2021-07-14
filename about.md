535c6827feb0458861049f19be7e2084f87da081
Jonathan Wright <j.wright72127@gmail.com>
2021-07-14 09:24:03 -0600

    Added Comments, An about, & minor HTML changes

---
+++ about.md
@@ -0,0 +1,9 @@
+# What is this Palindrome code about?
+
+- This code shows if the word you provided is read the same way front to back as it is back to front
+
+    - EX: <b>Racecar</b>
+
+## NOTE : This code does not work with spaces!
+
+- EX: <b>Race fast safe car</b> is a palindrome but while not be read as one!
--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+/**
+ * Checks Given Strings to see if they are Palindromes
+ * @param {*} str 
+ */
 function isPalindrome(str) {
-    let reversed = str.split("").reverse().join("");
-    let lowercaseStr = str.toLowerCase()
-    let lowercaseRev = reversed.toLowerCase()
+    let lowercaseStr = str.toLowerCase() // Makes the word inserted be lowercase
+    let reversed = lowercaseStr.split("").reverse().join(""); //The process of reverseing the string by spliting the letters apart, reversing it, and then putting the letters back together
 
-    if (lowercaseStr == lowercaseRev) {
-        console.log("Is a Palindrome!")
+    //Asks if reversed is the same word as the initial str provided
+    if (lowercaseStr == reversed) {
+        console.log("It's a Palindrome!") //The word was the same
     } else {
-        console.log("That is not a Palindrome")
+        console.log("It's not a Palindrome") //The word was not the same
     }
 }
 
-isPalindrome("BOB");
+isPalindrome("RaceCar"); //Sets up the Palindrome
